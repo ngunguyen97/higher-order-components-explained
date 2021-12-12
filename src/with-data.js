4635eb14ce241a37e5e1bdf5a5f4b5a26bcb0470
lucynguyen <ngu.nguyen@kyanon.digital>
2021-12-12 12:23:45 +0700

    Adding prettierrc file && destrucsturing state in with-data file

--- src/with-data.js
+++ src/with-data.js
@@ -1,31 +1,28 @@
 import React from 'react';
 
-const withData = WrappedComponent => {
+const withData = (WrappedComponent) => {
   class WithData extends React.Component {
     constructor(props) {
       super(props);
 
       this.state = {
-        data: []
+        data: [],
       };
     }
 
     componentDidMount() {
       setTimeout(() => {
         fetch(this.props.dataSource)
-          .then(response => response.json())
-          .then(data => this.setState({ data: data.slice(0, 3) }));
+          .then((response) => response.json())
+          .then((data) => this.setState({ data: data.slice(0, 3) }));
       }, 1500);
     }
 
     render() {
       const { dataSource, ...otherProps } = this.props;
+      const { data } = this.state;
 
-      return this.state.data.length < 1 ? (
-        <h1>LOADING</h1>
-      ) : (
-        <WrappedComponent data={this.state.data} {...otherProps} />
-      );
+      return data.length < 1 ? <h1>LOADING</h1> : <WrappedComponent data={data} {...otherProps} />;
     }
   }
 
